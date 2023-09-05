b34a6a20b328a504b9745e1fe8f2047232c2a64c
nitzanto <nitzan16001@gmail.com>
2023-09-05 14:30:06 +0300

    * Added a counter for variant samples

--- services/VariantData.py
+++ services/VariantData.py
@@ -1,6 +1,7 @@
 class VariantData:
     def __init__(self):
         self.columns = []
+        self.sampleCounter = {}
         self.limitReachedCount = 0
         self.samplesLimitReached = {}
         self.samples = []
